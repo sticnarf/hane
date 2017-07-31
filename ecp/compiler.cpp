86367d0eafdaff179c744241c382087cb51eba96
Yilin Chen <sticnarf@gmail.com>
2017-07-31 23:41:12 +0800

    Add ECP to CMakeLists

--- ecp/compiler.cpp
+++ ecp/compiler.cpp
@@ -1,4 +1,28 @@
-//
-// Created by Yilin Chen on 2017/7/31.
-//
+/**
+ * This compiler reads template from STDIN and outputs the C++ code to STDOUT.
+ * Also, two command line arguments must be given:
+ * the associated header file which is to be included,
+ * and the template class name.
+ */
 
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+std::string headerFile, className, templateText;
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        std::cerr << "Not enough command line arguments!" << std::endl;
+        return 1;
+    }
+    headerFile = argv[1];
+    className = argv[2];
+
+    char buf[2048];
+    while(fgets(buf, 2048, stdin) != nullptr) {
+        templateText += buf;
+    }
+
+    std::cout << templateText << std::endl;
+}
